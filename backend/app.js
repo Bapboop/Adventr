994a2276c59bfbef2b17cf2526cdd278766773c9
Robert <Robertpopphan@gmail.com>
2022-01-07 15:21:41 -0500

    Error handling code added

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,10 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 
+//Error handler for catching Sequelize errors:
+const { ValidationError } = require('sequelize');
+
+
 // Security Middleware
 if (!isProduction) {
   // enable cors only in development
@@ -39,8 +43,38 @@ app.use(
 // this must be below th csurf
 app.use(routes);
 
+// Error handling: placed after routes:
+//Catch unhandled requests and forward to error handler:
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource could not be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+})
 
 
+// Processes sequelize errors:
+app.use((err, _req, _res, next) => {
+  //check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+})
+
+//Error formatter: formats all errors before returning JSON response:
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.log(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
 
 
